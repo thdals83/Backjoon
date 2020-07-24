4b1d04108eec82fae1d1441a67becec91a110044
user <thdals83@naver.com>
2020-07-24 15:20:40 +0900

    2748 (파보나치 수2)

---
+++ 16/2748.py
@@ -0,0 +1,22 @@
+tmp=[0]*100
+
+def dp(x):
+    if(x==0): return 0;
+    if(x==1): return 1;
+    if(tmp[x]!=0): return tmp[x]
+    y=dp(x - 1) + dp(x - 2)
+    tmp[x] = y
+    return tmp[x]
+
+if __name__=="__main__":
+    x=int(input())
+    print(dp(x))
+
+'''
+n=int(input())
+m={0:0, 1:1}
+def f(n):
+    if n not in m: m[n] = f(n-1)+f(n-2)
+    return m[n]
+print(f(n))
+'''
