015bf624de2c1ad90680dcd1940a2f40fd82b838
user <thdals83@naver.com>
2020-08-12 11:36:52 +0900

    1149(RGB거리)

--- 16/1149.py
+++ 16/1149.py
@@ -6,9 +6,10 @@ for _ in range(n):
 
 for i in range(1, len(arr)):
     arr[i][0] = arr[i][0] + min(arr[i - 1][1], arr[i - 1][2])
-    print(arr[i][0])
+
     arr[i][1] = arr[i][1] + min(arr[i - 1][0], arr[i - 1][2])
-    print(arr[i][1])
+
     arr[i][2] = arr[i][2] + min(arr[i - 1][0], arr[i - 1][1])
-    print(arr[i][2])
+
+
 print(min(arr[n-1][0],arr[n-1][1],arr[n-1][2]))
