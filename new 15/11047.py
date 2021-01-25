e7b9801fc5c1425379939691840b277c1b28d162
thdals83 <thdals83@naver.com>
2021-01-25 16:38:06 +0900

    백준 11047(동전 0)

---
+++ new 15/11047.py
@@ -0,0 +1,17 @@
+n,k = map(int,input().split())
+
+arr=[]
+x=0
+
+for i in range(n):
+    arr.append((int(input())))
+
+for i in range(n-1,-1,-1):
+    if k==0:
+        break
+    if arr[i]>k:
+        continue
+    x += k//arr[i]
+    k%=arr[i]
+
+print(x)
